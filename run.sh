31a30b6fcded75652652894dcea3ad7ad609a626
FERRIERE Florian <ferriere.florian@gmail.com>
2015-07-02 15:37:18 +0200

    Add check if container wih same name exists

--- run.sh
+++ run.sh
@@ -15,6 +15,17 @@ if [ -n "$FFERRIERE_PG_SERVER_IMAGE" ]; then
     IMAGE="$FFERRIERE_PG_SERVER_IMAGE"
 fi
 
+NB_ROWS=$(docker ps | grep -w "$NAME" | wc -l)
+if [ "$NB_ROWS" -gt 0 ]; then
+    echo 'already started' >&2
+    exit 1
+fi
+
+NB_ROWS=$(docker ps -a | grep -w "$NAME" | wc -l)
+if [ "$NB_ROWS" -gt 0 ]; then
+    docker rm "$NAME"
+fi
+
 docker run -d \
   --name $NAME \
   --volumes-from $DATA_NAME \
